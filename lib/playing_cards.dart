15d7952d1b7e8cb8bf1914eef0699e76542152f2
Pintu Meena <pintulalmee@gmail.com>
2025-07-02 13:15:54 +0530

    minor update

--- lib/playing_cards.dart
+++ lib/playing_cards.dart
@@ -2,4 +2,6 @@ export 'src/views/playing_card_view.dart';
 export 'src/views/playing_card_view_style.dart';
 export 'src/views/flat_card_fan.dart';
 export 'src/util/card_aspect_ratio.dart';
-export 'package:playing_cards_lib/playing_cards_lib.dart';
+
+//Exporting the non ui library for playing cards, This avoids breaking changes on existing codebases.
+export 'package:playing_cards_lib/playing_cards_lib.dart'; 
